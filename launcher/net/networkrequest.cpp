66f6a0fe0bdee2aff941ce9575c59458c87bf1f9
PerssonBrown <Liuyx0808@outlook.com>
2023-01-17 12:18:01 +0800

    feat: send network requests

---
+++ launcher/net/networkrequest.cpp
@@ -0,0 +1,58 @@
+#include "networkrequest.h"
+#include <QUrl>
+#include <QObject>
+#include <QtNetwork>
+
+int NetworkRequest::sendPostRequest(const QString &strUrl, const QByteArray &data, QString &response) {
+    QMap<QString, QString> map;
+    return this->sendPostRequest(strUrl, data, map, response);
+}
+
+int NetworkRequest::sendPostRequest(const QString &strUrl, const QByteArray &data, const QMap<QString, QString> &map, QString &response) {
+    QNetworkAccessManager *manager = new QNetworkAccessManager();
+    QNetworkRequest request;
+    request.setUrl(QUrl(strUrl));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/json"));
+    for (QMap<QString, QString>::const_iterator it = map.begin(); it != map.end(); ++it) {
+        request.setRawHeader(it.key().toUtf8(), it.value().toUtf8());
+    }
+
+    QNetworkReply *reply = manager->post(request, data);
+    QEventLoop eventLoop;
+    connect(reply, SIGNAL(finished()), &eventLoop, SLOT(quit()));
+    eventLoop.exec();
+
+    if (reply->error() != QNetworkReply::NoError) {
+        QVariant statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+        return statusCode.toInt();
+    }
+    response = QString(reply->readAll());
+    return 1;
+}
+
+int NetworkRequest::sendGetRequest(const QString &strUrl, QString &response) {
+    QMap<QString, QString> map;
+    return this->sendGetRequest(strUrl, map, response);
+}
+
+int NetworkRequest::sendGetRequest(const QString &strUrl, const QMap<QString, QString> &map, QString &response) {
+    QNetworkAccessManager *manager = new QNetworkAccessManager();
+    QNetworkRequest request;
+    request.setUrl(QUrl(strUrl));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/json"));
+    for (QMap<QString, QString>::const_iterator it = map.begin(); it != map.end(); ++it) {
+        request.setRawHeader(it.key().toUtf8(), it.value().toUtf8());
+    }
+
+    QNetworkReply *reply = manager->get(request);
+    QEventLoop eventLoop;
+    connect(reply, SIGNAL(finished()), &eventLoop, SLOT(quit()));
+    eventLoop.exec();
+
+    if (reply->error() != QNetworkReply::NoError) {
+        QVariant statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+        return statusCode.toInt();
+    }
+    response = QString(reply->readAll());
+    return 1;
+}
---
+++ launcher/net/networkrequest.h
@@ -0,0 +1,15 @@
+#ifndef LUXLAUNCHER_NETWORKREQUEST_H
+#define LUXLAUNCHER_NETWORKREQUEST_H
+
+#include <QtCore>
+
+class NetworkRequest : public QObject {
+Q_OBJECT
+public:
+    int sendPostRequest(const QString &strUrl, const QByteArray &data, QString &response);
+    int sendPostRequest(const QString &strUrl, const QByteArray &data, const QMap<QString, QString> &map, QString &response);
+    int sendGetRequest(const QString &strUrl, QString &response);
+    int sendGetRequest(const QString &strUrl, const QMap<QString, QString> &map, QString &response);
+};
+
+#endif //LUXLAUNCHER_NETWORKREQUEST_H
